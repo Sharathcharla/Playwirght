43002fa31dde0c0da97b9ef4bff7ac29c2ba6d7f
Sharath Charlagorla <55597649+Sharathcharla@users.noreply.github.com>
2023-08-21 11:33:15 +0530

    Updated

--- Playwright_Practice/src/main/java/org/example/Main.java
+++ Playwright_Practice/src/main/java/org/example/Main.java
@@ -21,6 +21,7 @@ public static void main(String[] args) {
         page.waitForSelector("");
         // Click on the search result
         page.click("#search > div.s-desktop-width-max.s-desktop-content.s-wide-grid-style-t1.s-opposite-dir.s-wide-grid-style.sg-row > div.sg-col-20-of-24.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span.rush-component.s-latency-cf-section > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(6) > div > div > div > div > div > div.sg-col.sg-col-4-of-12.sg-col-8-of-16.sg-col-12-of-20.sg-col-12-of-24.s-list-col-right > div > div > div.a-section.a-spacing-none.puis-padding-right-small.s-title-instructions-style > h2 > a > span");
+        // Above selector will work often
         page.waitForLoadState();
 
         // Print the title of the product
